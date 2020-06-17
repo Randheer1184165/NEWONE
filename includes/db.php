ae07256e29f1a56fff6f62cb4bef64255ef237d6
Randheer Kumar <65314108+Randheer1184165@users.noreply.github.com>
2020-06-18 02:52:28 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,26 +1,29 @@
-<?php ob_start();
+<?php ob_start(); ?>
+<?php
 
-$db['db_host'] = "localhost:3307";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "cms";
+$db['db_host']="us-cdbr-east-05.cleardb.net:3306";
+$db['db_user']="ba5b503b7c7358";
+$db['db_pass']="c86e86ba";
+$db['db_name']="heroku_597cf2e5c9cb274";
 
-foreach($db as $key => $value){
+foreach ($db as $key => $value) {
+
 define(strtoupper($key), $value);
+
 }
 
-$connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
 
 
+$connection =mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
 
-$query = "SET NAMES utf8";
-mysqli_query($connection,$query);
+/*if ($connection) {
 
-//if($connection) {
-//
-//echo "We are connected";
-//
-//}
+echo "we are connected";
+	
+}else{
 
+	die(" we are sorry");
+	
+}*/
 
 ?>
