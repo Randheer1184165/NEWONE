71632a05e4b32787029a08f2758560c0713a921d
Randheer Kumar <65314108+Randheer1184165@users.noreply.github.com>
2020-06-18 02:55:25 +0530

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -20,7 +20,7 @@
                   
                   <?php 
 
-    $query = "SELECT * FROM categories LIMIT 3";
+    $query = "SELECT * FROM heroku_597cf2e5c9cb274.categories LIMIT 3";
     $select_all_categories_query = mysqli_query($connection,$query);
 
     while($row = mysqli_fetch_assoc($select_all_categories_query)) {
